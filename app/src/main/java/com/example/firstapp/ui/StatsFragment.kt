02d3ae2825a0d87dcf5b363b583d6297574926db
MeHow256 <m.m.dudziak.2@gmail.com>
2024-01-18 19:38:08 +0100

    Added chart logic without gps data handling

--- app/src/main/java/com/example/firstapp/ui/StatsFragment.kt
+++ app/src/main/java/com/example/firstapp/ui/StatsFragment.kt
@@ -14,8 +14,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.firstapp.R
 import com.example.firstapp.FileGPXIO
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
+import android.graphics.Color
+import com.github.mikephil.charting.components.XAxis
 
 class StatsFragment : Fragment() {
+    private lateinit var barChart: BarChart
     private val fileGpxIO = FileGPXIO()
     private lateinit var sharedViewModel: SharedViewModel
     companion object {
@@ -37,6 +45,9 @@ class StatsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        barChart = view.findViewById(R.id.barchart)
+
         val downloadButton = view.findViewById<Button>(R.id.download_button)
         val shareButton = view.findViewById<ImageButton>(R.id.share_button)
         downloadButton.setOnClickListener {
@@ -61,9 +72,9 @@ class StatsFragment : Fragment() {
 
         sharedViewModel.sessionStoppedLiveData.observe(viewLifecycleOwner) { sessionStopped ->
             if (sessionStopped) {
-                prepareChartData()
-
-                println("DUPA!!!!!!!!!!!!!!!!!!")
+                println("DATA TO HISTOGRAM!!!!!!!!!!!!!!!!!!:")
+                val histogramData = prepareChartData()
+                generateChart(histogramData)
 
                 sharedViewModel.setSessionStopped(false)
             }
@@ -90,39 +101,112 @@ class StatsFragment : Fragment() {
         )
     }
 
-    private fun prepareChartData() {
+//    private fun generateHistogram() {
+//        val values = mutableListOf<BarEntry>()
+//        for (i in 1..10) {
+//            val randomValue = (190f + (Math.random() * 20)).toFloat()
+//            values.add(BarEntry(randomValue, i.toFloat()))
+//        }
+//
+//        val barDataSet = BarDataSet(values, "Temp hist for test with random data")
+//        barDataSet.color = Color.BLUE
+//
+//        val dataSets = mutableListOf<IBarDataSet>()
+//        dataSets.add(barDataSet)
+//
+//        val data = BarData(dataSets)
+//        barChart.data = data
+//        barChart.setFitBars(true)
+//
+//        barChart.description.isEnabled = false
+//        barChart.setDrawValueAboveBar(true)
+//        barChart.xAxis.valueFormatter = null
+//        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+//        barChart.xAxis.setDrawGridLines(false)
+//        barChart.axisLeft.setDrawGridLines(false)
+//        barChart.axisRight.isEnabled = false
+//        barChart.animateY(1000)
+//    }
+
+    private fun generateChart(values: List<BarEntry>) {
+        val barDataSet = BarDataSet(values, "Number of laps of a given length")
+        barDataSet.color = Color.BLUE
+
+        val dataSets = mutableListOf<IBarDataSet>()
+        dataSets.add(barDataSet)
+
+        val data = BarData(dataSets)
+        barChart.data = data
+        barChart.setFitBars(true)
+
+        barChart.description.isEnabled = false
+        barChart.setDrawValueAboveBar(true)
+        barChart.xAxis.valueFormatter = null
+        barChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        barChart.xAxis.setDrawGridLines(false)
+        barChart.axisLeft.setDrawGridLines(false)
+        barChart.axisRight.isEnabled = false
+        barChart.animateY(1000)
+    }
+
+    private fun prepareChartData(): List<BarEntry> {
         val distanceArray = mutableListOf<Float>()
 
-        //TEMP
-
-        for (i in 1..10) {
-            val randomValue = (190f + (Math.random() * 20)).toFloat()
-            distanceArray.add(randomValue)
-        }
-
-        for (i in 11..20) {
-            val randomValue = (200f + (Math.random() * 100)).toFloat()
-            distanceArray.add(randomValue)
-        }
-
         sharedViewModel.runSession?.let { session ->
+            var latestLap: String? = null
+            var lastDistance: Int? = null
+
+            val lastDistanceList = mutableListOf<Int>()
+
             session.getMarkersList().forEachIndexed { _, marker ->
                 val snippetData = marker.snippet?.split(", ")
                 val lap = snippetData?.get(1)?.substring(6)
-                val distanceText = snippetData?.get(2)?.substring(0, snippetData[2].indexOf(" meters"))
-                val distanceValue = distanceText?.toFloatOrNull() ?: 0.0f
-                var latestLap: String? = "0"
-                var lastDistance: String? = "0"
+                val distanceText = snippetData?.get(2)?.substringBefore(" meters")
+                val distanceValue = distanceText?.toIntOrNull() ?: 0
 
                 if (lap != latestLap) {
-                    distanceArray.add(distanceValue)
+                    lastDistance?.let { lastDistanceList.add(it) }
+
                     latestLap = lap
+                    lastDistance = distanceValue
+                } else {
+                    lastDistance = distanceValue
                 }
-
-                println(lap)
-                println(distanceValue)
-
             }
+
+            lastDistance?.let { lastDistanceList.add(it) }
+
+            // Przykładowe DANE!! - trzeba wywalic jak bedzie dzialac przypisanie
+            for (i in 1..15) {
+                val randomValue = (180f + (Math.random() * 40)).toFloat()
+                distanceArray.add(randomValue)
+            }
+
+            lastDistanceList.forEach { distanceArray.add(it.toFloat()) }
         }
+
+
+        val amountOfOccurrences = countNearData(distanceArray)
+
+        println(distanceArray)
+        println(amountOfOccurrences)
+
+        return amountOfOccurrences.map { BarEntry(it.key, it.value.toFloat()) }
+    }
+
+    fun countNearData(lista: MutableList<Float>): Map<Float, Int> {
+        val diffInMeters = 1.0f
+        val amountOfApperances = mutableMapOf<Float, Int>()
+
+        for (dane in lista) {
+            val roundedData = dane.roundTo(diffInMeters)
+            amountOfApperances[roundedData] = amountOfApperances.getOrDefault(roundedData, 0) + 1
+        }
+
+        return amountOfApperances.toMap()
+    }
+
+    fun Float.roundTo(diffInMeters: Float): Float {
+        return (this / diffInMeters).toInt().toFloat() * diffInMeters
     }
 }
